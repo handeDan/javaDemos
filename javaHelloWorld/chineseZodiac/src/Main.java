4ec60f05b2688ac8ab4746c078c846140a20d463
hande <hande.dann@gmail.com>
2025-03-10 17:51:32 +0300

    chineseZodiac

---
+++ javaHelloWorld/chineseZodiac/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What year were you born?: ");
+        year = input.nextInt();
+
+        int zodiac = year%12;
+
+        switch (zodiac){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+        }
+
+    }
+}
